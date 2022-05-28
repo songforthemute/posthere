e7b2ac7ef8dfa448fed0ce184c6d030080f2bbe1
songforthemuto <huni0423@naver.com>
2022-05-28 22:16:22 +0900

    css works 2 & separate css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
-import "./styles.css";
+import "./styles/styles.css";
+import "./styles/postForm.css";
+import "./styles/post.css";
+import "./styles/profile.css";
+import "./styles/auth.css";
+import "./styles/navi.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
