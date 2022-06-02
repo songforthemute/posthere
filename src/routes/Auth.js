0f9da6a6aeea70c6c9f5cae4eadb17d56be1493b
songforthemuto <huni0423@naver.com>
2022-06-03 03:38:37 +0900

    Logo works

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -4,6 +4,7 @@ import AuthForm from "../components/AuthForm";
 const Auth = () => {
     return (
         <div className="authContainer">
+            <img src={process.env.REACT_APP_LOGO} alt="posthere" />
             <AuthForm />
         </div>
     );
